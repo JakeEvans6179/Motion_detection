0f321b153f500a0815b328317ca4dd0aab313abe
jakey <jakeyboyhk@icloud.com>
2025-11-19 14:22:03 +0000

    cleaned and added some explanations

--- background_subtraction.py
+++ background_subtraction.py
@@ -4,12 +4,31 @@ import numpy as np
 capture = cv.VideoCapture(0)
 subtractor = cv.createBackgroundSubtractorMOG2()
 
+'''
+Holds main logic for the motion detection system used in main.py
+
+Uses background and foreground separation through Gaussian Mixture-based Background/Foreground Segmentation Algorithm.
+foreground objects which are moving show up as a white blob on the image and a bounding box is drawn across each blob.
+
+To reduce the effect of noise we use cv.erode to first make the centre pixel black if pixels in a 5x5 grid around the point are not all white then use cv.dilate to make centre pixel white
+if one of the pixels in 5x5 grid are white to recover any lost blob from erode function.
+
+Situationally the camera will change focus/ white balance or if camera shakes there will be many bounding boxes, to account for this 
+when there are more than 25 bounding boxes drawn we do not analyse that frame,
+additionally bounding boxes smaller than a certain area will also be thrown away.
+
+We then take all the boxes and calculate the weighted average centre point (weighted by area) of all of them to find a single centre point of motion.
+This centre point is only plot after the object has been on the frame for 3 consecutive frames to reduce noise affecting the result, the program determines if they are the 
+same object by checking to see if they are within a radius of 30 pixels from the last point.
+'''
+
 
 #variables for motion tracker
 previous_center = None  #stores the coordinates (x,y) of last target
 object_age = 0  #how many frames in a row target has been tracked
 min_age_to_draw = 3  #how many frames before we draw
 distance_threshold = 30  #max distance between centre points
+kernel = np.ones((5, 5), np.uint8)
 
 fps = capture.get(cv.CAP_PROP_FPS)
 width = int(capture.get(cv.CAP_PROP_FRAME_WIDTH))
@@ -29,7 +48,7 @@ while capture.isOpened():
 
     #clean and get foreground mask
     fg_mask = subtractor.apply(frame)
-    kernel = np.ones((5, 5), np.uint8)
+
     fg_mask = cv.erode(fg_mask, kernel, iterations=1) #make centre pixel black if not all pixels in 5x5 grid are white (filter noise)
     fg_mask = cv.dilate(fg_mask, kernel, iterations=1) #make centre pixel white if one of the pixels in 5x5 grid are white (recover lost blobs)
 
